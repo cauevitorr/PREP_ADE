6d388037abdde83d95b7d0340db00c88d429b65a
Cauê Vitor <138812443+cauevitorr@users.noreply.github.com>
2024-11-25 14:45:15 -0300

    Delete restauranteBomBack directory

--- restauranteBomBack/src/config/conn.js
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from "sequelize";
-
-const conn = new Sequelize({
-  dialect: "sqlite",
-  storage: "./dev.sqlite",
-});
-
-export default conn;
--- restauranteBomBack/src/helper/get-token.js
+++
@@ -1,7 +0,0 @@
-const getToken = (request) => {
-    const auth = request.headers.authorization;
-    const token = auth.split(" ")[1]
-    return token
-};
-
-export default getToken;
